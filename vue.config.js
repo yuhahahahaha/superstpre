e28b084d60bae13573da0718ad2f3ca66c0ad455
unknown <1836051468@qq.com>
2022-04-05 00:47:40 +0800

    初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack:{
+    resolve:{
+      alias:{
+        // 默认Vue已经帮我们配置好'@':'src'了
+        'assets':'@assets',
+        'common':'@common',
+        'components':'@components',
+        'network':'@network',
+        'views':'@views'
+      }
+    }
+  }
 })
